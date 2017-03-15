c3f0d93c14d8282faa63bd0c2a445a19b935f7bd
metzgerenator <metzgerenator@gmail.com>
2017-03-14 23:07:12 -0400

    Added main dev screen and tab bar.  Need to finish implementation

---
+++ DotCode/ClientSummaryViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ClientSummaryViewController.swift
+//  DotCode
+//
+//  Created by Michael Metzger  on 3/14/17.
+//  Copyright © 2017 Mike. All rights reserved.
+//
+
+import UIKit
+
+class ClientSummaryViewController: UIViewController {
+
+    @IBOutlet var profilePhoto: UIImageView!
+    
+    @IBOutlet var pointOfContactPhoto: UIImageView!
+    
+    @IBOutlet var companyName: UILabel!
+    
+    @IBOutlet var location: UILabel!
+    
+    @IBOutlet var website: UILabel!
+    
+    
+    @IBOutlet var PointOfContact: UILabel!
+    
+    
+    
+    @IBOutlet var aboutUsLabel: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        let currentUser = CurrentUser()
+        currentUser.userAttributes { (Developer) in
+            
+            
+            
+        }
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
