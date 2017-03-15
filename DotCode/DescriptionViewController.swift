d61585dcf70057b30d7dd19e94d0ffdb632f29f8
metzgerenator <metzgerenator@gmail.com>
2017-03-14 22:35:10 -0400

    added non dev summary view

--- DotCode/DescriptionViewController.swift
+++ DotCode/DescriptionViewController.swift
@@ -2,7 +2,7 @@
 //  DescriptionViewController.swift
 //  ObjectiveCApp
 //
-//  Created by Aileen Taboy on 1/20/17.
+//  Created by Michael Metzger on 1/20/17.
 //  Copyright © 2017 Michael Metzger . All rights reserved.
 //
 
