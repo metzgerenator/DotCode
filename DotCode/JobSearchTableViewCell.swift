ed28062bfba0612246f50575896d1221b77b1e8f
metzgerenator <metzgerenator@gmail.com>
2017-03-26 21:11:20 -0400

    added inbox and search tabs to dev profile main

---
+++ DotCode/JobSearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  JobSearchTableViewCell.swift
+//  DotCode
+//
+//  Created by Michael Metzger  on 3/26/17.
+//  Copyright © 2017 Mike. All rights reserved.
+//
+
+import UIKit
+
+class JobSearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
